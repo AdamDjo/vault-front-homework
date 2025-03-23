5dbf102a29f85b7c5f263ee98e8a6c3b0b131732
Adam BenM <adambenmessaoud2@gmail.com>
2025-03-23 13:31:22 +0100

    style(transaction-list): hide scrollbar

--- src/features/transactions/components/TransactionList.tsx
+++ src/features/transactions/components/TransactionList.tsx
@@ -10,13 +10,13 @@ type Props = {
 const TransactionList = ({ transactions }: Props) => {
   return (
     <>
-      <Box className="relative overflow-y-auto gap-3  flex flex-col  h-[400px] py-6 ">
+      <Box className="relative overflow-y-auto gap-3  flex flex-col  h-[400px] py-6 no-scrollbar">
         {transactions.map((transaction) => {
           const mappedProps = mapTransactionToUI(transaction);
           return <TransactionItem key={transaction.id} {...mappedProps} />;
         })}
       </Box>
-      <div className="pointer-events-none absolute bottom-0 rounded-4xl left-0 right-0 h-16 bg-gradient-to-t from-white to-white/0"></div>
+      <div className="pointer-events-none absolute bottom-0 rounded-4xl left-0 right-0 h-30 bg-gradient-to-t from-white to-white/0"></div>
     </>
   );
 };
