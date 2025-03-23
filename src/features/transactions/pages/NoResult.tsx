ae595bfc17893ff6eec553109e8062cc3462811f
Adam BenM <adambenmessaoud2@gmail.com>
2025-03-23 13:53:09 +0100

    fix(Notfound page): fix ui no result page

--- src/features/transactions/pages/NoResult.tsx
+++ src/features/transactions/pages/NoResult.tsx
@@ -1,13 +1,15 @@
+import Box from '@/shared/ui/Box';
+
 const NoResult = () => {
   return (
-    <div className="flex flex-col items-center justify-center  text-center space-y-2">
+    <Box className="flex flex-col items-center h-[400px] justify-center gap-4  text-center space-y-2">
       <img
         src="/images/no-result.svg"
         alt="No result found"
-        className="w-32 h-32"
+        className="max-w-32 max-h-32"
       />
       <h2 className="text-base font-semibold text-black">No result found...</h2>
-    </div>
+    </Box>
   );
 };
 
